1e48f194b41d45603d9aba97826fc3850a1e72cf
sleepp-dev <117117726+sleepp-dev@users.noreply.github.com>
2022-10-31 13:35:16 -0700

    Update bingoItems.js

--- src/utils/bingoItems.js
+++ src/utils/bingoItems.js
@@ -4,13 +4,13 @@ let bingoItems = [
 	'Elysian player gets banned for whatever reason',
 	'You crash/dc and get put in a queue',
 	'Yonas gets locked by AGS due to population',
-	'Somebody has \"420\" and \"69\" in their name',
+	'Somebody has "420" and "69" in their name',
 	'Somebody in chat complains about Greatsword being OP',
 	'Somebody asks how to get into Brimstone in global chat',
 	'Somebody links [Golden Scarab] in global/trade chat',
 	'Somebody asks where to find iron/fiber/hemp/ in global chat',
-	'Somebody mentions \"Mankirk\'s wife\" in chat',
-	'Somebody yells \"LEEEROY JENKINS\" in discord appropriately',
+	'Somebody mentions "Mankirk\'s wife" in chat',
+	'Somebody yells "LEEEROY JENKINS" in discord appropriately',
 	'Town makes less than 4000g in the first week',
 	'Participate in a mass portal run or chest run in the first week',
 	'Somebody spiking their mic in the middle of EF making your ears bleed',
@@ -19,7 +19,7 @@ let bingoItems = [
 	'An edgy-sounding Syndicate company takes a territory',
 	'A religious-sounding Covenant company takes a territory',
 	'Territory flips twice in 7 days',
-	'Town flips and new company doesn\'t upgrade anything',
+	'Town flips and new company doesn't upgrade anything',
 	'Notable person is banned from the game',
 	'Bots are back',
 	'Somebody reaches 600 GS',
@@ -29,5 +29,6 @@ let bingoItems = [
 	'Someone spams their Twitch in global chat',
 	'Someone creates a server-wide discord',
 	'A /dice gambling ring forms',
+]
 ]
 export default bingoItems;
